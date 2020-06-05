a4ec489220da43887e74d399a6e550d4d0458511
Muhammad Yasir Javed <k163614@nu.edu.pk>
2020-06-05 19:20:57 +0500

    master push

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export default val="http://9f20a0261b31.ngrok.io"
-=======
-export default val="http://f630eb365a5e.ngrok.io";
->>>>>>> d06fa835ceaf7fa1c2bf271ba0f13f5757a9f0b1
+export default val="http://f630eb365a5e.ngrok.io"
